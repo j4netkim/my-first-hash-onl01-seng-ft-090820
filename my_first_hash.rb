10b5992e5f60ad2998caadeb66f26e77d6e75fcb
J K <kj688895@gmail.com>
2020-09-18 20:56:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,8 @@ def adding
                   "oil paintings" => 3
                  }
 
-shipping_manifest[:"muskets"] = 2
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
 end
 
 puts shipping_manifest
