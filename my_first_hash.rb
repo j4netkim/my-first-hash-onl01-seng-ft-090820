247f2387a30f179d5e3dd76553ab7a3d858fc7d9
J K <kj688895@gmail.com>
2020-09-18 21:02:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,7 +27,10 @@ def adding
                  }
 
   shipping_manifest["muskets"] = 2
+  shipping_manifest["muskets"]
+
   shipping_manifest["gun powder"] = 4
+  shipping_manifest["gun powder"]
 
   puts shipping_manifest
-end 
+end
