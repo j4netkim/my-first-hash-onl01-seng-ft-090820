99eb6e5844f9cf048d2673668c36f92b9f539b6c
J K <kj688895@gmail.com>
2020-09-18 20:49:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,12 +27,11 @@ def adding
                  }
 
 shipping_manifest["muskets"] = 2
+shipping_manifest["muskets"]
+
 shipping_manifest["gun powder"] = 4
+shipping_manifest["gun powder"]
 
-puts shipping_manifest
-
-
-  # add 4 gun powder to the shipping_manifest hash below
 
 
 
